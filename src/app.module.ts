19e92954906e0466182035bff0d3359edcc34d0b
Rifki Rahadian <rrahadian94@gmail.com>
2024-05-16 10:32:37 +0700

    chore: database definition using module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { dataBaseConfig } from './database/database.config';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [SequelizeModule.forRoot(dataBaseConfig)],
+  imports: [DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.provider.ts
@@ -0,0 +1,17 @@
+import { Sequelize } from 'sequelize-typescript';
+import { User } from 'src/user/entities/user.entity';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: '.db/data.sqlite3',
+      });
+      sequelize.addModels([User]);
+      await sequelize.sync();
+      return sequelize;
+    },
+  },
+];
