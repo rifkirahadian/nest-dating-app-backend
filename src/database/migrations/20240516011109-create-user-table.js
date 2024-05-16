aa7e551ac38e07f93fadbf53f0a6fdb52c50dcbc
Rifki Rahadian <rrahadian94@gmail.com>
2024-05-16 08:26:47 +0700

    chore: user table migration

---
+++ src/database/migrations/20240516011109-create-user-table.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isVerified: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      dailySwipeQuota: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      updatedAt: Sequelize.DATE,
+      createdAt: Sequelize.DATE,
+    });
+  },
+
+  async down(queryInterface) {
+    return queryInterface.dropTable('Users');
+  },
+};
