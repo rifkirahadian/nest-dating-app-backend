200a7114910fb390145e859d7df80e2c399e091d
Rifki Rahadian <rrahadian94@gmail.com>
2024-05-19 20:23:26 +0700

    test: should create swipes

--- src/swipe/swipe.service.spec.ts
+++ src/swipe/swipe.service.spec.ts
@@ -3,12 +3,25 @@ import { SwipeService } from './swipe.service';
 import { swipesProviders } from './swipe.provider';
 import { usersProviders } from '../user/user.provider';
 import { UserService } from '../user/user.service';
+import { SwipeDto } from './dto/swipe.dto';
+import { Swipe } from './entities/swipe.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 describe('SwipeService', () => {
   let service: SwipeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRootAsync({
+          useFactory: async () => ({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            synchronize: true,
+            models: [Swipe],
+          }),
+        }),
+      ],
       providers: [
         SwipeService,
         ...swipesProviders,
@@ -31,5 +44,24 @@ describe('SwipeService', () => {
       .mockImplementationOnce(() => Promise.resolve(count));
 
     expect(await service.getCountTodaySwipes(1)).toEqual(count);
+  });
+
+  it('should create swipes', async () => {
+    const swipeDto: SwipeDto = {
+      userId: 3,
+      type: 'like',
+    };
+
+    const newSwipe: Swipe = Swipe.build({
+      userId: 1,
+      userTargetId: 2,
+      type: 'like',
+    });
+
+    jest
+      .spyOn(service, 'create')
+      .mockImplementationOnce(() => Promise.resolve(newSwipe));
+
+    expect(await service.create(swipeDto, newSwipe.userId)).toEqual(newSwipe);
   });
 });
