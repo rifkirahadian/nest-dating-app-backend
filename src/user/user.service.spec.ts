1d6d75cf6fda4785fb3937732b037c7f37f7013d
Rifki Rahadian <rrahadian94@gmail.com>
2024-05-19 13:46:04 +0700

    test: should create a new user

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
-import { usersProviders } from './user.provider';
 import { JwtService } from '@nestjs/jwt';
-
+import { RegisterDto } from './dto/register.dto';
+import { User } from './entities/user.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { usersProviders } from './user.provider';
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRootAsync({
+          useFactory: async () => ({
+            dialect: 'sqlite',
+            storage: ':memory:',
+            synchronize: true,
+            models: [User],
+          }),
+        }),
+      ],
       providers: [UserService, ...usersProviders, JwtService],
     }).compile();
 
@@ -16,5 +28,33 @@ describe('UserService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new user', async () => {
+    const registerDto: RegisterDto = {
+      name: 'Test User',
+      email: 'test@example.com',
+      password: 'password123',
+      gender: 'male',
+    };
+
+    const newUser = new User();
+    newUser.id = 1;
+    newUser.name = 'Test User';
+    newUser.email = 'test@example.com';
+    newUser.password = 'password123';
+    newUser.gender = 'male';
+    newUser.isVerified = false;
+    newUser.dailySwipeQuota = 10;
+
+    jest
+      .spyOn(service, 'create')
+      .mockImplementationOnce(() => Promise.resolve(newUser));
+
+    // Call the create method of the UserService with the registerDto
+    const createdUser = await service.create(registerDto);
+
+    // Verify that the created user matches the expected newUser
+    expect(createdUser).toEqual(newUser);
   });
 });
